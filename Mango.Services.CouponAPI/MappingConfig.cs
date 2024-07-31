423e523f26cc740b1eea4901dfe9fec1790c662d
jaspreet <jaspreet@gigaprofits.com>
2024-07-30 22:28:22 -0400

    Add project files.

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Models;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+
+    }
+}
